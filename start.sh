9a616e27dc2b3e4f6bf4c51c9bc6cc13484f39a0
stasstoun <stasstoun>
2026-08-03 22:50:41 +0000

    change in sh file

--- start.sh
+++ start.sh
@@ -2,6 +2,8 @@
 
 python manage.py migrate
 
+python manage.py collectstatic --noinput
+
 python manage.py import_dictionary dictionary.csv
 
 gunicorn bookreader_core.wsgi:application --bind 0.0.0.0:$PORT
