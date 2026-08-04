17d1ebc2c171072922fd7cc1494f5ca6adaaaccd
stasstoun <stasstoun>
2026-08-04 12:12:14 +0000

    date

--- start.sh
+++ start.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
 date
+echo "migrate"
 python manage.py migrate
 
 date
+echo "collectstatic"
 python manage.py collectstatic --noinput
 
 date
+echo "import"
 python manage.py import_dictionary dictionary.csv
 
 date
+echo "starting gunicorn"
+
 gunicorn bookreader_core.wsgi:application --bind 0.0.0.0:$PORT
