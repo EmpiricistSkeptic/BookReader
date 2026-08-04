b88e9e4e01bee771ac9b10145f09512d7b0342e3
stasstoun <stasstoun>
2026-08-04 12:04:24 +0000

    check time

--- start.sh
+++ start.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+date
 python manage.py migrate
 
+date
 python manage.py collectstatic --noinput
 
+date
 python manage.py import_dictionary dictionary.csv
 
+date
 gunicorn bookreader_core.wsgi:application --bind 0.0.0.0:$PORT
